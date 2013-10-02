2b0332790ac777937e9042672e19a71805d4ac31
bob <bobbijvoet@gmail.com>
2013-10-02 23:00:10 +0200

    Updated bower, updated .gitignore, removed touch module dependency

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
 
 window.mobileDevice = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) )
 
-var app = angular.module('AlbumTube', ['ngResource', 'AlbumTubeServices', 'touch'])
+var app = angular.module('AlbumTube', ['ngResource', 'AlbumTubeServices'])
 	.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.otherwise({
